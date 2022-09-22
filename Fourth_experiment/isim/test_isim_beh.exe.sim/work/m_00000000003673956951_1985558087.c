a6b2ea8dfa6db5131406f583e7f618892a221cd0
Alan Xu <xdpcsyy@gmail.com>
2022-09-22 22:13:28 +0800

    feat(four_experiment): add four_experiment
    add four_experiment

---
+++ Fourth_experiment/isim/test_isim_beh.exe.sim/work/m_00000000003673956951_1985558087.c
@@ -0,0 +1,120 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "F:/Desktop/FPGA/Fourth_experiment/test.v";
+static unsigned int ng1[] = {1U, 0U};
+static int ng2[] = {1, 0};
+static int ng3[] = {0, 0};
+static unsigned int ng4[] = {286331153U, 0U};
+static unsigned int ng5[] = {2U, 0U};
+
+
+
+static void Initial_53_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+
+LAB0:    t1 = (t0 + 3488U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(53, ng0);
+
+LAB4:    xsi_set_current_line(55, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(56, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(58, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 2088);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(59, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 2248);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(60, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 2408);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(61, ng0);
+    t2 = ((char*)((ng4)));
+    t3 = (t0 + 2568);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    xsi_set_current_line(64, ng0);
+    t2 = (t0 + 3296);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB5;
+
+LAB1:    return;
+LAB5:    xsi_set_current_line(65, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(66, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(67, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(68, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 2088);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(69, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 2248);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(70, ng0);
+    t2 = ((char*)((ng5)));
+    t3 = (t0 + 2408);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 5);
+    xsi_set_current_line(71, ng0);
+    t2 = ((char*)((ng4)));
+    t3 = (t0 + 2568);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 32);
+    goto LAB1;
+
+}
+
+
+extern void work_m_00000000003673956951_1985558087_init()
+{
+	static char *pe[] = {(void *)Initial_53_0};
+	xsi_register_didat("work_m_00000000003673956951_1985558087", "isim/test_isim_beh.exe.sim/work/m_00000000003673956951_1985558087.didat");
+	xsi_register_executes(pe);
+}
